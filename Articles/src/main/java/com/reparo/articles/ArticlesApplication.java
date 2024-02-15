a6c674cbb1d8e6a3e7ea524565e8bca93b8d79f0
ClementLegouest <clement.legouest@gmail.com>
2024-02-15 23:16:50 +0100

    Articles base

---
+++ Articles/src/main/java/com/reparo/articles/ArticlesApplication.java
@@ -0,0 +1,26 @@
+package com.reparo.articles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ArticlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+	
+	@GetMapping("/articles")
+	public String ReadArticles() {
+		return "Articles";
+	}
+}
